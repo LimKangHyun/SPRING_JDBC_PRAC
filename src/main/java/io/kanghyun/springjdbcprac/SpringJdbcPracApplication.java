881b131d26bc4be36389e4a47b6acfc926cfd931
limkanghyun <limkh06910691@gmail.com>
2025-04-09 23:22:21 +0900

    Initial commit

---
+++ src/main/java/io/kanghyun/springjdbcprac/SpringJdbcPracApplication.java
@@ -0,0 +1,13 @@
+package io.kanghyun.springjdbcprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kanghyun/springjdbcprac/SpringJdbcPracApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kanghyun.springjdbcprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
