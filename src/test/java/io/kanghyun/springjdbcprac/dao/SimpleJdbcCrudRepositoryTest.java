142604e5c71b856fc4c9eda2956be8aada92b966
limkanghyun <limkh06910691@gmail.com>
2025-04-10 17:25:00 +0900

    test: 회원가입(save) 테스트 - Hikari CP로 커넥션 풀 사용

---
+++ src/test/java/io/kanghyun/springjdbcprac/dao/SimpleJdbcCrudRepositoryTest.java
@@ -0,0 +1,52 @@
+package io.kanghyun.springjdbcprac.dao;
+
+import com.zaxxer.hikari.HikariDataSource;
+import io.kanghyun.springjdbcprac.member.Member;
+import io.kanghyun.springjdbcprac.util.ConnectionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class SimpleJdbcCrudRepositoryTest {
+
+    // 구현체보다 인터페이스를 통해 테스트를 하는것이 좋다.
+    // 상위타입을 통한 테스트가 좋음 -> 구현이 아닌 역할(기능)에 의존하는 것이 이점이 많다.
+    SimpleCrudRepository repository;
+
+    @BeforeEach
+    void init() {
+        // Hikari CP는
+        // 자바에서 DB랑 연결할 때 Connection을 직접 만들고 닫고 하면
+        //→ 성능 저하 + 리소스 낭비가 크기 때문에
+        //커넥션 풀(Pool)을 사용해서 미리 일정 수의 연결을 만들어두고 재사용하는 거야.
+        // HikariDataSource는 Hikari CP의 구현체
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(ConnectionUtil.MysqlDbConnectionConstant.URL);
+        dataSource.setUsername(ConnectionUtil.MysqlDbConnectionConstant.USERNAME);
+        dataSource.setPassword(ConnectionUtil.MysqlDbConnectionConstant.PASSWORD);
+
+        repository = new SimpleJdbcCrudRepository(dataSource);
+    }
+
+    @Test
+    @DisplayName("save test")
+    void save_test() throws Exception {
+
+        String randomUserStr = "USER_" + (int) (Math.random() * 100_000);
+        String randomUserPwd = randomUserStr + "_PWD";
+        log.info(randomUserStr);
+
+        //회원가입 요청
+        // member_id는 0으로 초기화만 진행 -> 실제로는 auto_increment의 숫자가 매핑되어 db에 적용됨
+        Member saveReq = new Member(0, randomUserStr, randomUserPwd);
+        Member savedMember = repository.save(saveReq);
+
+        log.info("saved member : {}", savedMember);
+        Assertions.assertThat(savedMember.getMemberId()).isEqualTo(0);
+
+    }
+
+}
